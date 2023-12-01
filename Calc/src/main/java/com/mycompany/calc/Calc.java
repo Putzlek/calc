c7829cdc12b5220d28ec4a0e6d58747295e757ab
Alex Sander <141284564+Putzlek@users.noreply.github.com>
2023-12-01 20:48:11 -0300

    Add files via upload

---
+++ Calc/src/main/java/com/mycompany/calc/Calc.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calc;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class Calc {
+
+    public static void main(String[] args) {
+        Calculadora calculo = new Calculadora();
+        calculo.setEnabled(true);
+    }
+}
